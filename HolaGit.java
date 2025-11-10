3fa9c2981ad18ce0b0eb2872209fe1abba942f1a
mvoytkovych-jpg <mvoytkovych@alumnes.ilerna.com>
2025-11-10 21:16:11 +0100

    Modificació fitxer Java

--- HolaGit.java
+++ HolaGit.java
@@ -4,8 +4,8 @@ public class exercici2 {
 
     public static void main(Strings[] args) {
 
-        System.out.println("Hola Git");
-
+        System.out.println("Heloo World");
+        
     }
 
 }
